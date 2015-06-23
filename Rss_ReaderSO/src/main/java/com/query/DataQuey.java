ff509dcf006be60499852bc969bf92585b278c18
vanessa <vanessagia91@gmail.com>
2015-06-23 08:48:08 -0500

    Login

---
+++ Rss_ReaderSO/src/main/java/com/query/DataQuey.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.query;
+
+import com.entity.Login;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author vanessa
+ */
+public class DataQuey {
+     EntityManagerFactory emf;
+     EntityManager em;
+
+    public DataQuey() {
+        emf = Persistence.createEntityManagerFactory("postgres");
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+    }
+    public boolean  loginControl(String username , String password){
+        try {
+            Login l = em.createNamedQuery("Login.control", Login.class).setParameter("username", username).setParameter("password", password).getSingleResult();
+            if(l!= null){
+                 return true;
+            }
+            return false;
+        } catch (Exception e) {
+        }   return false;
+    }
+     
+    
+}
---
+++ Rss_ReaderSO/target/generated-sources/annotations/com/entity/Login_.java
@@ -0,0 +1,15 @@
+package com.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-06-23T08:36:50")
+@StaticMetamodel(Login.class)
+public class Login_ { 
+
+    public static volatile SingularAttribute<Login, Integer> id;
+    public static volatile SingularAttribute<Login, String> username;
+    public static volatile SingularAttribute<Login, String> password;
+
+}
